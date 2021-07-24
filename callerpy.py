20aa7ef66f9a15453c91729e47e58a27d576d836
Nikhil Das <nknikhilporuppath@gmail.com>
2021-07-24 21:17:02 +0530

    Removed Bloat

--- callerpy.py
+++ callerpy.py
@@ -17,36 +17,21 @@ start_message = client.messages.create(
 )
 print(start_message.sid)
 while True:
-    url1 = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=302&date='
-    url1 = url1 + str(xxx.day) + '-' + str(xxx.month) + '-' + str(xxx.year)
-    url2 = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=308&date='
-    url2 = url2 + str(xxx.day) + '-' + str(xxx.month) + '-' + str(xxx.year)
-    rx = requests.get(url1)
-    ry = requests.get(url2)
+    url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=302&date='
+    url = url + str(xxx.day) + '-' + str(xxx.month) + '-' + str(xxx.year)
+    response = requests.get(url)
     try:
-        opx = rx.json()['centers']
-        opy = ry.json()['centers']
+        locations = response.json()['centers']
     except:
         continue
 
-    for i in opx:
+    for i in locations:
         x = i['sessions']
         for xx in x:
             if xx['available_capacity_dose1'] > 0:
                 message = client.messages.create(
                     messaging_service_sid='MG2d51afac03d6c38d303ecebf76d089e9',
                     body='Available Slot in Malappuram',
-                    to=''   # Receiver Mobile number with Country Code
-                )
-                print(message.sid)
-                time.sleep(2 * 60)
-    for i in opy:
-        x = i['sessions']
-        for xx in x:
-            if xx['available_capacity_dose1'] > 0:
-                message = client.messages.create(
-                    messaging_service_sid='MG2d51afac03d6c38d303ecebf76d089e9',
-                    body='Available Slot in Palakkad',
                     to=''   # Receiver Mobile number with Country Code
                 )
                 print(message.sid)
